41135db57705b4c23a5065b20c4d1721e38109ac
YangJipsa <109192582+yangjipsa@users.noreply.github.com>
2025-10-22 21:06:33 +0900

    Add LED control sketch for active high output

---
+++ _1_digitalOutput_led_activeHigh.ino
@@ -0,0 +1,19 @@
+int led1 = 2
+int led2 = 3
+
+void setup()
+{
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  delay(2000);
+
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  delay(1000);
+}
