1fffb7aba3cce9609326f7626df01d3a3c1c2ae6
YangJipsa <109192582+yangjipsa@users.noreply.github.com>
2025-10-24 23:09:34 +0900

    Create _3_analogOutput_led.ino

---
+++ _3_analogOutput_led.ino
@@ -0,0 +1,26 @@
+int led1 = 5
+int led2 = 6
+
+void setup()
+{
+
+}
+
+void loop()
+{
+  analogWrite(led1, 0);
+  analogWrite(led2, 0);
+  delay(1000);
+
+  analogWrite(led1, 80);
+  analogWrite(led2, 80);
+  delay(1000);
+
+  analogWrite(led1, 150);
+  analogWrite(led2, 150);
+  delay(1000);
+
+  analogWrite(led1, 255);
+  analogWrite(led2, 255);
+  delay(1000);
+}
