879cd12661b49575f81ab87f30fd159276e25ceb
YangJipsa <109192582+yangjipsa@users.noreply.github.com>
2025-10-26 09:07:19 +0900

    Update _4_analogOutput_DCmotor.ino

--- _4_analogOutput_DCmotor.ino
+++ _4_analogOutput_DCmotor.ino
@@ -1,1 +1,31 @@
+int pinF = 6;
+int pinR = 5;
 
+void setup()
+{
+  pinMode(pinF, OUTPUT);
+  pinMode(pinR, OUTPUT);
+}
+
+void loop()
+{
+  // Forward 
+  analogWrite(pinF, 100);
+  digitalWRite(pinR, LOW);
+  delay(1000);
+
+  // Stop
+  digitalWrite(pinF, LOW);
+  digitalWrite(pinR, LOW);
+  delay(1000);
+
+  // Backward
+  digitalWrite(pinF, LOW);
+  analogWrite(pinR, 200);
+  delay(1000);
+  
+  // Break
+  digitalWrite(pinF, HIGH);
+  digitalWrite(pinR, HIGH);
+  delay(1000);
+}
