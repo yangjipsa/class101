4f4e3545e5538d358738e936223ffb750f2c31d6
YangJipsa <109192582+yangjipsa@users.noreply.github.com>
2025-10-26 23:50:21 +0900

    Create _5_analogInput_Brightness.ino

---
+++ _5_analogInput_Brightness.ino
@@ -0,0 +1,13 @@
+int Brightness = 0;
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int valBrightness = analogRead(Brightness);
+  Serial.println(valBrightness);
+  delay(100);
+}
