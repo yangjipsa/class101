a90aded5532a67602c5828a6d97825390d9a6d74
YangJipsa <109192582+yangjipsa@users.noreply.github.com>
2025-12-09 09:06:54 +0900

    Update _6_analogInput_practice.ino

--- _6_analogInput_practice.ino
+++ _6_analogInput_practice.ino
@@ -1,1 +1,23 @@
+// LED(Active LOW) : D5, D6
+// Light Sensor : A0
+int led1 = 5;
+int led2 = 6;
+int Brightness = A0;
+int MAX_PWM = 255;
 
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  int valBrightness = analogRead(Brightness);
+  int valPWM = MAX_PWM - valBrightness/4;
+  // 주변이 밝으면 LED는 어두워지고
+  // 주변이 어두워지면 LED는 밝아지는 동작
+  analogWrite(led1, valPWM);
+  analogWrite(led2, valPWM);
+  
+  delay(100);
+}
